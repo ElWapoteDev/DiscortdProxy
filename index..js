5af08110ba21b06fff49b736c397df50bf3a009a
Charman Troll  <elguapodehuapos12@gmail.com>
2024-02-16 21:01:14 -0600

    Revertido

--- index..js
+++ index..js
@@ -1,3 +1,4 @@
+// src/app.js
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -8,5 +9,5 @@ const miRuta = require('./routes/dc');
 // Usa el router con un path base
 app.use('/api', miRuta);
 
-// Exporta tu aplicación Express para Vercel
-module.exports = app;
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
